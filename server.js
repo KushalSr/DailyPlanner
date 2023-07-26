31e4c500c90213950a3ac16a023c6ee24f5fcb26
KushalSr <kushalsrivastava10@gmail.com>
2023-07-27 02:52:51 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import {app} from "./app.js";
+import {connectDB} from "./database/database.js";
+
+connectDB();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(` Server is working on port: ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
+    });
